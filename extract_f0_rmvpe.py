c1e2ca7fbc49f274ed90893488e6272e0f480255
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-07-06 19:37:37 +0300

    Update extract_f0_rmvpe.py

--- extract_f0_rmvpe.py
+++ extract_f0_rmvpe.py
@@ -44,6 +44,13 @@ class FeatureInput(object):
                 print("loading rmvpe model")
                 self.model_rmvpe = RMVPE("rmvpe.pt", is_half=True, device="cuda")
             f0 = self.model_rmvpe.infer_from_audio(x, thred=0.03)
+        if f0_method == "rmvpe+":
+            if hasattr(self, "model_rmvpe") == False:
+                from lib.rmvpe import RMVPE
+
+                print("loading rmvpe model")
+                self.model_rmvpe = RMVPE("rmvpe.pt", is_half=False, device="cuda")
+            f0 = self.model_rmvpe.infer_from_audio_modified(x, thred=0.02)
         return f0
 
     def coarse_f0(self, f0):
