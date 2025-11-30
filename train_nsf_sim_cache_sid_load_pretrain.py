2d61e73c659995f12c12b5a384485093acf255be
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-11-30 15:43:18 +0300

    Update train_nsf_sim_cache_sid_load_pretrain.py

--- train_nsf_sim_cache_sid_load_pretrain.py
+++ train_nsf_sim_cache_sid_load_pretrain.py
@@ -36,7 +36,7 @@ if hps.vocoder == "RefineGAN":
     from lib.infer_pack.models_refinegan import (
         SynthesizerTrnMs768NSFsid as RVC_Model_f0,
         SynthesizerTrnMs768NSFsid_nono as RVC_Model_nof0,
-        MultiPeriodDiscriminatorV3 as MultiPeriodDiscriminator,
+        MultiPeriodDiscriminatorV2 as MultiPeriodDiscriminator,
     )
 if hps.vocoder == "Hifi-GAN":
     from lib.infer_pack.models import (
@@ -446,7 +446,7 @@ def train_and_evaluate(
             # Generator
             y_d_hat_r, y_d_hat_g, fmap_r, fmap_g = net_d(wave, y_hat)
             with autocast(enabled=False):
-                loss_mel = MultiScaleMelSpectrogramLoss(sample_rate=hps.data.sampling_rate) (y_mel, y_hat_mel) * hps.train.c_mel
+                loss_mel = F.l1_loss(y_mel, y_hat_mel) * hps.train.c_mel
                 loss_kl = kl_loss(z_p, logs_q, m_p, logs_p, z_mask) * hps.train.c_kl
                 loss_fm = feature_loss(fmap_r, fmap_g)
                 loss_gen, losses_gen = generator_loss(y_d_hat_g)
